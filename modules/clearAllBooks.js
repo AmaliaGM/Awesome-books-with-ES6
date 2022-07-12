4220615187eb72887443d143126d773d90d3ba1a
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-07-12 05:57:35 -0300

    default import

--- modules/clearAllBooks.js
+++ modules/clearAllBooks.js
@@ -1,4 +1,4 @@
-export function clearBooks() {
+export default function clearBooks() {
   const bookList = document.getElementById('book-list');
   while (bookList.firstChild) {
     bookList.removeChild(bookList.firstChild);
